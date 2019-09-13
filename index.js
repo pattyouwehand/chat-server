89b4244c26b8ca8af24dab84295ff6937846822d
pattyouwehand <pattyouwehand@gmail.com>
2019-09-13 10:22:13 +0200

    setup function port

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 
-app.listen(4000)
+function onlisten(){
+  console.log(`Listening on post ${port}`)
+}
+
+app.listen(port, onlisten)
