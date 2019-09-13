a36d87f4f1b411fa45d49c390e28e922cd4f1201
pattyouwehand <pattyouwehand@gmail.com>
2019-09-13 10:59:02 +0200

    add message

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const Sse = require('json-sse')         //place to store clients and send message to all clients
+const bodyParser = require('body-parser')
 const factory = require('./router')
 const stream = new Sse()
 const app = express()
 const router = factory(stream)          // is alway going to use the same stream instead off a reference
 const port = process.env.PORT || 4000
+
+const jsonParser = bodyParser.json()
+app.use(jsonParser)
 
 function onlisten(){
   console.log(`Listening on post ${port}`)
--- router.js
+++ router.js
@@ -8,12 +8,24 @@ function factory(stream){
 
   function onStream(req, res){
     const data = JSON.stringify(messages)
+    console.log('data test:', data)
 
     stream.updateInit(data)                 //use json sse, so need to convert json to string
-    stream.init(req, res)
+    return stream.init(req, res)
   }
   router.get('/stream', onStream)
 
+  function onMessage(req, res){
+    const {text} = req.body
+
+    messages.push(text)
+    const data = JSON.stringify(messages)
+    stream.send(data)
+
+    return res.send(text)                     //don't forget this return!!
+
+  }
+  router.post('/message', onMessage)
   return router
 }
 
